b22d0e7eb93807b9f15089a6ccd2ac027db54a62
allenrahbar <allen.rahbar@gmail.com>
2021-09-09 18:56:59 +0000

    add: flip card function and style

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// Select all cards and flip card function
+
+const cards = document.querySelectorAll('memory-card');
+
+//* Flip card function help with code from https://www.w3schools.com/jsref/prop_element_classlist.asp
+
+function flipCard() {
+    this.classlist.toggle('flip')
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard))
