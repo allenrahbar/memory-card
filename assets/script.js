c90266c0f2374616144f1d7516efef3f2e891150
allenrahbar <allen.rahbar@gmail.com>
2021-09-14 11:04:18 +0000

    add: function for match cards and data to html

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,23 @@
-// Select all cards and flip card function
+// Variables for game
 
 const cards = document.querySelectorAll('.memory-card');
 
-//* Flip card function help with code from https://www.w3schools.com/jsref/prop_element_classlist.asp
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+// Flip card function help with code from https://www.w3schools.com/jsref/prop_element_classlist.asp
+// Function for matching cards
 
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+
