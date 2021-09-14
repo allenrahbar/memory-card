71452b67f7d06d8d2c58746fe056661a8e3c1e8f
allenrahbar <allen.rahbar@gmail.com>
2021-09-14 11:11:52 +0000

    add: function for disable cards and unflip cards

--- assets/script.js
+++ assets/script.js
@@ -14,8 +14,31 @@ function flipCard() {
     if(!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
+        return;
     }
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
 }
+
+function checkForMatch () {
+    if (firstCard.dataset.animals === secondCard.dataset.animals) {
+        disableCards()
+        return;
+    }
+    unflipCards()
+}
+function disableCards () {
+    firstCard.removeEventListener ('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+function unflipCards () {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
+}
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
