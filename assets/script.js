ce93441ceb37727687f5727c7f51693a93695eb5
allenrahbar <allen.rahbar@gmail.com>
2021-09-14 11:18:49 +0000

    add: lockboard var to game

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,14 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard= false;
 let firstCard, secondCard;
 
 // Flip card function help with code from https://www.w3schools.com/jsref/prop_element_classlist.asp
 // Function for matching cards
 
 function flipCard() {
+    if (lockBoard) return;
     this.classList.add('flip');
 
     if(!hasFlippedCard) {
@@ -26,16 +28,20 @@ function checkForMatch () {
         disableCards()
         return;
     }
-    unflipCards()
-}
+    
 function disableCards () {
     firstCard.removeEventListener ('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
 }
 function unflipCards () {
+    lockBoard = true;
+
+// code taken from https://www.w3schools.com/jsref/met_win_settimeout.asp
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+        lockBoard= false;
     }, 1500);
 }
 
